ee27cad438b6c9dcc446d81d8d7ff4e7e0c3218b
Locria Cyber <74560659+locriacyber@users.noreply.github.com>
2023-01-04 22:18:23 +0000

    tried trunk. failed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,14 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import wasm from "vite-plugin-wasm";
 import topLevelAwait from "vite-plugin-top-level-await";
+import { ViteRsw } from 'vite-plugin-rsw';
 
 /** @type {import('vite').UserConfig} */
 const config = {
 	plugins: [
-            wasm(),
-            topLevelAwait(),
+	    ViteRsw(),
+            //topLevelAwait(),
+            //wasm(),
             sveltekit()
 	]
 };
