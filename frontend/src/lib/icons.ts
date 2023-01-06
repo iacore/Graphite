35cd4e4aa2b033684ebb78c16b32c1af7ee7843c
Locria Cyber <74560659+locriacyber@users.noreply.github.com>
2023-01-06 04:49:35 +0000

    Add icons
    Verbatim copy from vue impl

--- frontend/src/lib/icons.ts
+++ frontend/src/lib/icons.ts
@@ -1,7 +1,289 @@
+/* eslint-disable import/first */
+
+// Graphics
+import GraphiteLogotypeSolid from "$icon/other/graphite-logotype-solid.svg?raw";
+
+const GRAPHICS = {
+	GraphiteLogotypeSolid: { component: GraphiteLogotypeSolid, size: undefined },
+} as const;
+
+// 12px Solid
+import Add from "$icon/12px-solid/add.svg?raw";
+import Checkmark from "$icon/12px-solid/checkmark.svg?raw";
+import CloseX from "$icon/12px-solid/close-x.svg?raw";
+import DropdownArrow from "$icon/12px-solid/dropdown-arrow.svg?raw";
+import Edit from "$icon/12px-solid/edit.svg?raw";
+import Empty12px from "$icon/12px-solid/empty-12px.svg?raw";
+import FullscreenEnter from "$icon/12px-solid/fullscreen-enter.svg?raw";
+import FullscreenExit from "$icon/12px-solid/fullscreen-exit.svg?raw";
+import Grid from "$icon/12px-solid/grid.svg?raw";
+import Info from "$icon/12px-solid/info.svg?raw";
+import KeyboardArrowDown from "$icon/12px-solid/keyboard-arrow-down.svg?raw";
+import KeyboardArrowLeft from "$icon/12px-solid/keyboard-arrow-left.svg?raw";
+import KeyboardArrowRight from "$icon/12px-solid/keyboard-arrow-right.svg?raw";
+import KeyboardArrowUp from "$icon/12px-solid/keyboard-arrow-up.svg?raw";
+import KeyboardBackspace from "$icon/12px-solid/keyboard-backspace.svg?raw";
+import KeyboardCommand from "$icon/12px-solid/keyboard-command.svg?raw";
+import KeyboardControl from "$icon/12px-solid/keyboard-control.svg?raw";
+import KeyboardEnter from "$icon/12px-solid/keyboard-enter.svg?raw";
+import KeyboardOption from "$icon/12px-solid/keyboard-option.svg?raw";
+import KeyboardShift from "$icon/12px-solid/keyboard-shift.svg?raw";
+import KeyboardSpace from "$icon/12px-solid/keyboard-space.svg?raw";
+import KeyboardTab from "$icon/12px-solid/keyboard-tab.svg?raw";
+import Link from "$icon/12px-solid/link.svg?raw";
+import Overlays from "$icon/12px-solid/overlays.svg?raw";
+import Remove from "$icon/12px-solid/remove.svg?raw";
+import ResetColors from "$icon/12px-solid/reset-colors.svg?raw";
+import Snapping from "$icon/12px-solid/snapping.svg?raw";
+import Swap from "$icon/12px-solid/swap.svg?raw";
+import VerticalEllipsis from "$icon/12px-solid/vertical-ellipsis.svg?raw";
+import Warning from "$icon/12px-solid/warning.svg?raw";
+import WindowButtonWinClose from "$icon/12px-solid/window-button-win-close.svg?raw";
+import WindowButtonWinMaximize from "$icon/12px-solid/window-button-win-maximize.svg?raw";
+import WindowButtonWinMinimize from "$icon/12px-solid/window-button-win-minimize.svg?raw";
+import WindowButtonWinRestoreDown from "$icon/12px-solid/window-button-win-restore-down.svg?raw";
+
+const SOLID_12PX = {
+	Add: { component: Add, size: 12 },
+	Checkmark: { component: Checkmark, size: 12 },
+	CloseX: { component: CloseX, size: 12 },
+	DropdownArrow: { component: DropdownArrow, size: 12 },
+	Edit: { component: Edit, size: 12 },
+	Empty12px: { component: Empty12px, size: 12 },
+	FullscreenEnter: { component: FullscreenEnter, size: 12 },
+	FullscreenExit: { component: FullscreenExit, size: 12 },
+	Grid: { component: Grid, size: 12 },
+	Info: { component: Info, size: 12 },
+	KeyboardArrowDown: { component: KeyboardArrowDown, size: 12 },
+	KeyboardArrowLeft: { component: KeyboardArrowLeft, size: 12 },
+	KeyboardArrowRight: { component: KeyboardArrowRight, size: 12 },
+	KeyboardArrowUp: { component: KeyboardArrowUp, size: 12 },
+	KeyboardBackspace: { component: KeyboardBackspace, size: 12 },
+	KeyboardCommand: { component: KeyboardCommand, size: 12 },
+	KeyboardControl: { component: KeyboardControl, size: 12 },
+	KeyboardEnter: { component: KeyboardEnter, size: 12 },
+	KeyboardOption: { component: KeyboardOption, size: 12 },
+	KeyboardShift: { component: KeyboardShift, size: 12 },
+	KeyboardSpace: { component: KeyboardSpace, size: 12 },
+	KeyboardTab: { component: KeyboardTab, size: 12 },
+	Link: { component: Link, size: 12 },
+	Overlays: { component: Overlays, size: 12 },
+	Remove: { component: Remove, size: 12 },
+	ResetColors: { component: ResetColors, size: 12 },
+	Snapping: { component: Snapping, size: 12 },
+	Swap: { component: Swap, size: 12 },
+	VerticalEllipsis: { component: VerticalEllipsis, size: 12 },
+	Warning: { component: Warning, size: 12 },
+	WindowButtonWinClose: { component: WindowButtonWinClose, size: 12 },
+	WindowButtonWinMaximize: { component: WindowButtonWinMaximize, size: 12 },
+	WindowButtonWinMinimize: { component: WindowButtonWinMinimize, size: 12 },
+	WindowButtonWinRestoreDown: { component: WindowButtonWinRestoreDown, size: 12 },
+} as const;
+
+// 16px Solid
+import AlignBottom from "$icon/16px-solid/align-bottom.svg?raw";
+import AlignHorizontalCenter from "$icon/16px-solid/align-horizontal-center.svg?raw";
+import AlignLeft from "$icon/16px-solid/align-left.svg?raw";
+import AlignRight from "$icon/16px-solid/align-right.svg?raw";
+import AlignTop from "$icon/16px-solid/align-top.svg?raw";
+import AlignVerticalCenter from "$icon/16px-solid/align-vertical-center.svg?raw";
+import BooleanDifference from "$icon/16px-solid/boolean-difference.svg?raw";
+import BooleanIntersect from "$icon/16px-solid/boolean-intersect.svg?raw";
+import BooleanSubtractBack from "$icon/16px-solid/boolean-subtract-back.svg?raw";
+import BooleanSubtractFront from "$icon/16px-solid/boolean-subtract-front.svg?raw";
+import BooleanUnion from "$icon/16px-solid/boolean-union.svg?raw";
+import CheckboxChecked from "$icon/16px-solid/checkbox-checked.svg?raw";
+import CheckboxUnchecked from "$icon/16px-solid/checkbox-unchecked.svg?raw";
+import Copy from "$icon/16px-solid/copy.svg?raw";
+import EyeHidden from "$icon/16px-solid/eye-hidden.svg?raw";
+import EyeVisible from "$icon/16px-solid/eye-visible.svg?raw";
+import Eyedropper from "$icon/16px-solid/eyedropper.svg?raw";
+import File from "$icon/16px-solid/file.svg?raw";
+import FlipHorizontal from "$icon/16px-solid/flip-horizontal.svg?raw";
+import FlipVertical from "$icon/16px-solid/flip-vertical.svg?raw";
+import Folder from "$icon/16px-solid/folder.svg?raw";
 import GraphiteLogo from "$icon/16px-solid/graphite-logo.svg?raw";
+import NodeArtboard from "$icon/16px-solid/node-artboard.svg?raw";
+import NodeBlur from "$icon/16px-solid/node-blur.svg?raw";
+import NodeBrushwork from "$icon/16px-solid/node-brushwork.svg?raw";
+import NodeColorCorrection from "$icon/16px-solid/node-color-correction.svg?raw";
+import NodeFolder from "$icon/16px-solid/node-folder.svg?raw";
+import NodeGradient from "$icon/16px-solid/node-gradient.svg?raw";
+import NodeImage from "$icon/16px-solid/node-image.svg?raw";
+import NodeImaginate from "$icon/16px-solid/node-imaginate.svg?raw";
+import NodeMagicWand from "$icon/16px-solid/node-magic-wand.svg?raw";
+import NodeMask from "$icon/16px-solid/node-mask.svg?raw";
+import NodeMotionBlur from "$icon/16px-solid/node-motion-blur.svg?raw";
+import NodeNodes from "$icon/16px-solid/node-nodes.svg?raw";
+import NodeOutput from "$icon/16px-solid/node-output.svg?raw";
+import NodeShape from "$icon/16px-solid/node-shape.svg?raw";
+import NodeText from "$icon/16px-solid/node-text.svg?raw";
+import NodeTransform from "$icon/16px-solid/node-transform.svg?raw";
+import Paste from "$icon/16px-solid/paste.svg?raw";
+import Random from "$icon/16px-solid/random.svg?raw";
+import Regenerate from "$icon/16px-solid/regenerate.svg?raw";
+import Reload from "$icon/16px-solid/reload.svg?raw";
+import Rescale from "$icon/16px-solid/rescale.svg?raw";
+import Reset from "$icon/16px-solid/reset.svg?raw";
+import Settings from "$icon/16px-solid/settings.svg?raw";
+import Trash from "$icon/16px-solid/trash.svg?raw";
+import ViewModeNormal from "$icon/16px-solid/view-mode-normal.svg?raw";
+import ViewModeOutline from "$icon/16px-solid/view-mode-outline.svg?raw";
+import ViewModePixels from "$icon/16px-solid/view-mode-pixels.svg?raw";
+import ViewportDesignMode from "$icon/16px-solid/viewport-design-mode.svg?raw";
+import ViewportGuideMode from "$icon/16px-solid/viewport-guide-mode.svg?raw";
+import ViewportSelectMode from "$icon/16px-solid/viewport-select-mode.svg?raw";
+import ZoomIn from "$icon/16px-solid/zoom-in.svg?raw";
+import ZoomOut from "$icon/16px-solid/zoom-out.svg?raw";
+import ZoomReset from "$icon/16px-solid/zoom-reset.svg?raw";
 
-const ALL_ICONS: Record<string, string> = {
-	GraphiteLogo,
-};
+const SOLID_16PX = {
+	AlignBottom: { component: AlignBottom, size: 16 },
+	AlignHorizontalCenter: { component: AlignHorizontalCenter, size: 16 },
+	AlignLeft: { component: AlignLeft, size: 16 },
+	AlignRight: { component: AlignRight, size: 16 },
+	AlignTop: { component: AlignTop, size: 16 },
+	AlignVerticalCenter: { component: AlignVerticalCenter, size: 16 },
+	BooleanDifference: { component: BooleanDifference, size: 16 },
+	BooleanIntersect: { component: BooleanIntersect, size: 16 },
+	BooleanSubtractBack: { component: BooleanSubtractBack, size: 16 },
+	BooleanSubtractFront: { component: BooleanSubtractFront, size: 16 },
+	BooleanUnion: { component: BooleanUnion, size: 16 },
+	CheckboxChecked: { component: CheckboxChecked, size: 16 },
+	CheckboxUnchecked: { component: CheckboxUnchecked, size: 16 },
+	Copy: { component: Copy, size: 16 },
+	Eyedropper: { component: Eyedropper, size: 16 },
+	EyeHidden: { component: EyeHidden, size: 16 },
+	EyeVisible: { component: EyeVisible, size: 16 },
+	File: { component: File, size: 16 },
+	FlipHorizontal: { component: FlipHorizontal, size: 16 },
+	FlipVertical: { component: FlipVertical, size: 16 },
+	Folder: { component: Folder, size: 16 },
+	GraphiteLogo: { component: GraphiteLogo, size: 16 },
+	NodeArtboard: { component: NodeArtboard, size: 16 },
+	NodeBlur: { component: NodeBlur, size: 16 },
+	NodeBrushwork: { component: NodeBrushwork, size: 16 },
+	NodeColorCorrection: { component: NodeColorCorrection, size: 16 },
+	NodeFolder: { component: NodeFolder, size: 16 },
+	NodeGradient: { component: NodeGradient, size: 16 },
+	NodeImage: { component: NodeImage, size: 16 },
+	NodeImaginate: { component: NodeImaginate, size: 16 },
+	NodeMagicWand: { component: NodeMagicWand, size: 16 },
+	NodeMask: { component: NodeMask, size: 16 },
+	NodeMotionBlur: { component: NodeMotionBlur, size: 16 },
+	NodeNodes: { component: NodeNodes, size: 16 },
+	NodeOutput: { component: NodeOutput, size: 16 },
+	NodeShape: { component: NodeShape, size: 16 },
+	NodeText: { component: NodeText, size: 16 },
+	NodeTransform: { component: NodeTransform, size: 16 },
+	Paste: { component: Paste, size: 16 },
+	Random: { component: Random, size: 16 },
+	Regenerate: { component: Regenerate, size: 16 },
+	Reload: { component: Reload, size: 16 },
+	Rescale: { component: Rescale, size: 16 },
+	Reset: { component: Reset, size: 16 },
+	Settings: { component: Settings, size: 16 },
+	Trash: { component: Trash, size: 16 },
+	ViewModeNormal: { component: ViewModeNormal, size: 16 },
+	ViewModeOutline: { component: ViewModeOutline, size: 16 },
+	ViewModePixels: { component: ViewModePixels, size: 16 },
+	ViewportDesignMode: { component: ViewportDesignMode, size: 16 },
+	ViewportGuideMode: { component: ViewportGuideMode, size: 16 },
+	ViewportSelectMode: { component: ViewportSelectMode, size: 16 },
+	ZoomIn: { component: ZoomIn, size: 16 },
+	ZoomOut: { component: ZoomOut, size: 16 },
+	ZoomReset: { component: ZoomReset, size: 16 },
+} as const;
 
-export default ALL_ICONS;
+// 16px Two-Tone
+import MouseHintDrag from "$icon/16px-two-tone/mouse-hint-drag.svg?raw";
+import MouseHintLmbDrag from "$icon/16px-two-tone/mouse-hint-lmb-drag.svg?raw";
+import MouseHintLmb from "$icon/16px-two-tone/mouse-hint-lmb.svg?raw";
+import MouseHintMmbDrag from "$icon/16px-two-tone/mouse-hint-mmb-drag.svg?raw";
+import MouseHintMmb from "$icon/16px-two-tone/mouse-hint-mmb.svg?raw";
+import MouseHintNone from "$icon/16px-two-tone/mouse-hint-none.svg?raw";
+import MouseHintRmbDrag from "$icon/16px-two-tone/mouse-hint-rmb-drag.svg?raw";
+import MouseHintRmb from "$icon/16px-two-tone/mouse-hint-rmb.svg?raw";
+import MouseHintScrollDown from "$icon/16px-two-tone/mouse-hint-scroll-down.svg?raw";
+import MouseHintScrollUp from "$icon/16px-two-tone/mouse-hint-scroll-up.svg?raw";
+
+const TWO_TONE_16PX = {
+	MouseHintDrag: { component: MouseHintDrag, size: 16 },
+	MouseHintLmb: { component: MouseHintLmb, size: 16 },
+	MouseHintLmbDrag: { component: MouseHintLmbDrag, size: 16 },
+	MouseHintMmb: { component: MouseHintMmb, size: 16 },
+	MouseHintMmbDrag: { component: MouseHintMmbDrag, size: 16 },
+	MouseHintNone: { component: MouseHintNone, size: 16 },
+	MouseHintRmb: { component: MouseHintRmb, size: 16 },
+	MouseHintRmbDrag: { component: MouseHintRmbDrag, size: 16 },
+	MouseHintScrollDown: { component: MouseHintScrollDown, size: 16 },
+	MouseHintScrollUp: { component: MouseHintScrollUp, size: 16 },
+} as const;
+
+// 24px Two-Tone
+import GeneralArtboardTool from "$icon/24px-two-tone/general-artboard-tool.svg?raw";
+import GeneralEyedropperTool from "$icon/24px-two-tone/general-eyedropper-tool.svg?raw";
+import GeneralFillTool from "$icon/24px-two-tone/general-fill-tool.svg?raw";
+import GeneralGradientTool from "$icon/24px-two-tone/general-gradient-tool.svg?raw";
+import GeneralNavigateTool from "$icon/24px-two-tone/general-navigate-tool.svg?raw";
+import GeneralSelectTool from "$icon/24px-two-tone/general-select-tool.svg?raw";
+import RasterBrushTool from "$icon/24px-two-tone/raster-brush-tool.svg?raw";
+import RasterCloneTool from "$icon/24px-two-tone/raster-clone-tool.svg?raw";
+import RasterDetailTool from "$icon/24px-two-tone/raster-detail-tool.svg?raw";
+import RasterHealTool from "$icon/24px-two-tone/raster-heal-tool.svg?raw";
+import RasterImaginateTool from "$icon/24px-two-tone/raster-imaginate-tool.svg?raw";
+import RasterNodesTool from "$icon/24px-two-tone/raster-nodes-tool.svg?raw";
+import RasterPatchTool from "$icon/24px-two-tone/raster-patch-tool.svg?raw";
+import RasterRelightTool from "$icon/24px-two-tone/raster-relight-tool.svg?raw";
+import VectorEllipseTool from "$icon/24px-two-tone/vector-ellipse-tool.svg?raw";
+import VectorFreehandTool from "$icon/24px-two-tone/vector-freehand-tool.svg?raw";
+import VectorLineTool from "$icon/24px-two-tone/vector-line-tool.svg?raw";
+import VectorPathTool from "$icon/24px-two-tone/vector-path-tool.svg?raw";
+import VectorPenTool from "$icon/24px-two-tone/vector-pen-tool.svg?raw";
+import VectorRectangleTool from "$icon/24px-two-tone/vector-rectangle-tool.svg?raw";
+import VectorShapeTool from "$icon/24px-two-tone/vector-shape-tool.svg?raw";
+import VectorSplineTool from "$icon/24px-two-tone/vector-spline-tool.svg?raw";
+import VectorTextTool from "$icon/24px-two-tone/vector-text-tool.svg?raw";
+
+const TWO_TONE_24PX = {
+	GeneralArtboardTool: { component: GeneralArtboardTool, size: 24 },
+	GeneralEyedropperTool: { component: GeneralEyedropperTool, size: 24 },
+	GeneralFillTool: { component: GeneralFillTool, size: 24 },
+	GeneralGradientTool: { component: GeneralGradientTool, size: 24 },
+	GeneralNavigateTool: { component: GeneralNavigateTool, size: 24 },
+	GeneralSelectTool: { component: GeneralSelectTool, size: 24 },
+	RasterImaginateTool: { component: RasterImaginateTool, size: 24 },
+	RasterNodesTool: { component: RasterNodesTool, size: 24 },
+	RasterBrushTool: { component: RasterBrushTool, size: 24 },
+	RasterCloneTool: { component: RasterCloneTool, size: 24 },
+	RasterDetailTool: { component: RasterDetailTool, size: 24 },
+	RasterHealTool: { component: RasterHealTool, size: 24 },
+	RasterPatchTool: { component: RasterPatchTool, size: 24 },
+	RasterRelightTool: { component: RasterRelightTool, size: 24 },
+	VectorEllipseTool: { component: VectorEllipseTool, size: 24 },
+	VectorFreehandTool: { component: VectorFreehandTool, size: 24 },
+	VectorLineTool: { component: VectorLineTool, size: 24 },
+	VectorPathTool: { component: VectorPathTool, size: 24 },
+	VectorPenTool: { component: VectorPenTool, size: 24 },
+	VectorRectangleTool: { component: VectorRectangleTool, size: 24 },
+	VectorShapeTool: { component: VectorShapeTool, size: 24 },
+	VectorSplineTool: { component: VectorSplineTool, size: 24 },
+	VectorTextTool: { component: VectorTextTool, size: 24 },
+} as const;
+
+// All icons
+const ICON_LIST = {
+	...GRAPHICS,
+	...SOLID_12PX,
+	...SOLID_16PX,
+	...TWO_TONE_16PX,
+	...TWO_TONE_24PX,
+} as const;
+
+// Exported icons and types
+export const ICONS: Record<keyof typeof ICON_LIST, { component: string; size: number | undefined }> = ICON_LIST;
+export const ICON_COMPONENTS = Object.fromEntries(Object.entries(ICONS).map(([name, data]) => [name, data.component]));
+
+export type IconName = keyof typeof ICONS;
+
+export default ICON_COMPONENTS;
